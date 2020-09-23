9131d1b204cebcf0cb3b8dcb25418bc80e869665
Jake Wharton <jw@squareup.com>
2020-09-23 16:26:50 -0400

    Only render when there are state changes

--- mosaic/src/main/kotlin/example/Main.kt
+++ mosaic/src/main/kotlin/example/Main.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.EmbeddingContext
 import androidx.compose.runtime.Recomposer
 import androidx.compose.runtime.compositionFor
 import androidx.compose.runtime.dispatch.BroadcastFrameClock
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.withMutableSnapshot
 import androidx.compose.runtime.yoloGlobalEmbeddingContext
 import com.facebook.yoga.YogaConstants.UNDEFINED
@@ -16,11 +18,10 @@ import com.jakewharton.mosaic.Text
 import com.jakewharton.mosaic.renderToString
 import kotlinx.coroutines.CoroutineStart.UNDISPATCHED
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import kotlin.coroutines.CoroutineContext
 
 fun main() {
@@ -41,57 +42,58 @@ fun main() {
 		}
 		yoloGlobalEmbeddingContext = embeddingContext
 
-		val recomposer = Recomposer(embeddingContext)
-
 		val rootNode = BoxNode()
-		val applier = MosaicNodeApplier(rootNode)
+		fun render() {
+			val root = rootNode.yoga
+			root.calculateLayout(UNDEFINED, UNDEFINED)
+			println(rootNode.renderToString())
+		}
 
+		val applier = MosaicNodeApplier(rootNode)
+		val recomposer = Recomposer(embeddingContext)
 		val composition = compositionFor(Any(), applier, recomposer)
 		composeContext[Job]!!.invokeOnCompletion {
 			composition.dispose()
 		}
 
-		withContext(composeContext) {
-			launch(start = UNDISPATCHED) {
-				recomposer.runRecomposeAndApplyChanges()
-			}
-
-			val counts = mutableStateOf(0)
-			composition.setContent {
-//				val count by remember { counts }
-Column {
-  Row {
-    Text("one")
-    Text("two")
-  }
-  Row {
-    Text("three")
-    Text("four")
-  }
-}
-//				Text("The count is $count")
-			}
-
-			launch(start = UNDISPATCHED) {
-				while (true) {
-					clock.sendFrame(System.nanoTime())
-					val root = rootNode.yoga
-					root.calculateLayout(UNDEFINED, UNDEFINED)
-					println(rootNode.renderToString())
-					delay(500)
+		val counts = mutableStateOf(0)
+		composition.setContent {
+			val count by remember { counts }
+			Column {
+				Row {
+					Text("one")
+					Text("two")
+				}
+				Row {
+					Text("three$count")
+					Text("four")
 				}
 			}
-
-			for (i in 1..10) {
-				delay(1_000)
-				withMutableSnapshot {
-					counts.value = i * 10
-				}
-			}
-			// Wait for final frame.
-			clock.withFrameNanos { }
-
-			cancel()
 		}
+
+		launch(start = UNDISPATCHED, context = composeContext) {
+			render()
+			while (true) {
+				recomposer.recomposeAndApplyChanges(1L)
+				render()
+			}
+		}
+
+		launch(start = UNDISPATCHED, context = composeContext) {
+			while (true) {
+				clock.sendFrame(System.nanoTime())
+				delay(100)
+			}
+		}
+
+		for (i in 1..10) {
+			delay(1_000)
+			withMutableSnapshot {
+				counts.value = i * 10
+			}
+		}
+
+		// TODO how do we wait for the final frame?
+		coroutineContext[Job]!!.cancelChildren()
 	}
 }
