9b1880fe474f661a1c002b5e2e9638ceab25c959
Jake Wharton <jw@squareup.com>
2020-09-25 11:43:41 -0400

    Create an object to represent a mosaic
    This will let it be driven in tests in a deterministic manner.

--- mosaic/src/main/kotlin/com/jakewharton/mosaic/mosaic.kt
+++ mosaic/src/main/kotlin/com/jakewharton/mosaic/mosaic.kt
@@ -17,12 +17,23 @@ import org.fusesource.jansi.AnsiConsole
 import java.util.concurrent.TimeUnit.NANOSECONDS
 import kotlin.coroutines.CoroutineContext
 
-private fun CoroutineScope.launchMosaic(
-	content: @Composable () -> Unit,
-	output: (String) -> Unit,
-): Job {
-	val mainThread = Thread.currentThread()
+interface Mosaic {
+	/**
+	 * Trigger a frame which will apply any pending changes to the output.
+	 *
+	 * @return True if the string representation should be re-rendered as it has probably changed.
+	 */
+	fun sendFrame(): Boolean
 
+	fun cancel()
+
+	/** Measure, layout, and render the content for display. */
+	override fun toString(): String
+}
+
+fun CoroutineScope.createMosaic(
+	content: @Composable () -> Unit,
+): Mosaic {
 	var dirty = true
 	val clock = BroadcastFrameClock {
 		dirty = true
@@ -31,6 +42,7 @@ private fun CoroutineScope.launchMosaic(
 	val job = Job(coroutineContext[Job])
 	val composeContext = coroutineContext + clock + job
 
+	val mainThread = Thread.currentThread()
 	val embeddingContext = object : EmbeddingContext {
 		override fun isMainThread(): Boolean {
 			return Thread.currentThread() === mainThread
@@ -43,15 +55,10 @@ private fun CoroutineScope.launchMosaic(
 	yoloGlobalEmbeddingContext = embeddingContext
 
 	val rootNode = BoxNode()
-
 	val applier = MosaicNodeApplier(rootNode)
 
 	val recomposer = Recomposer(embeddingContext)
 	val composition = compositionFor(Any(), applier, recomposer)
-
-	job.invokeOnCompletion {
-		composition.dispose()
-	}
 
 	// Start undispatched to ensure we can use suspending things inside the content.
 	launch(start = UNDISPATCHED, context = composeContext) {
@@ -60,21 +67,25 @@ private fun CoroutineScope.launchMosaic(
 
 	composition.setContent(content)
 
-	launch(context = composeContext) {
-		while (true) {
-			clock.sendFrame(System.nanoTime())
-			if (dirty) {
-				dirty = false
+	return object : Mosaic {
+		override fun sendFrame(): Boolean {
+			clock.sendFrame(0L) // Frame time value is not used by Compose runtime.
 
-				val root = rootNode.yoga
-				root.calculateLayout(UNDEFINED, UNDEFINED)
-				output(rootNode.renderToString())
-			}
-			delay(100)
+			val wasDirty = dirty
+			dirty = false
+			return wasDirty
+		}
+
+		override fun cancel() {
+			job.cancel()
+			composition.dispose()
+		}
+
+		override fun toString(): String {
+			rootNode.yoga.calculateLayout(UNDEFINED, UNDEFINED)
+			return rootNode.renderToString()
 		}
 	}
-
-	return job
 }
 
 /**
@@ -86,13 +97,15 @@ private const val ansiConsole = true
 fun CoroutineScope.launchMosaic(
 	content: @Composable () -> Unit,
 ): Job {
+	val mosaic = createMosaic(content)
+
 	if (ansiConsole) {
 		AnsiConsole.systemInstall()
 	}
 
 	var lastHeight = 0
 	var lastRenderNanos = 0L
-	val job = launchMosaic(content) { output ->
+	fun render(output: String) {
 		if (ansiConsole) {
 			repeat(lastHeight) {
 				print(ansi().cursorUpLine().eraseLine())
@@ -116,8 +129,17 @@ fun CoroutineScope.launchMosaic(
 		println(output)
 	}
 
-	if (ansiConsole) {
-		job.invokeOnCompletion {
+	val job = launch {
+		while (true) {
+			if (mosaic.sendFrame()) {
+				render(mosaic.toString())
+			}
+			delay(100)
+		}
+	}
+	job.invokeOnCompletion {
+		mosaic.cancel()
+		if (ansiConsole) {
 			AnsiConsole.systemUninstall()
 		}
 	}
