ce9db14cb04d2844d8450d95b84b12fe33e96c1f
Jake Wharton <jw@squareup.com>
2020-10-20 11:19:06 -0400

    Check for svg-term and fail gracefully

--- update-example-svgs.sh
+++ update-example-svgs.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if ! command -v svg-term &> /dev/null; then
+    echo "Command 'svg-term' not found. Please install with 'npm install -g svg-term-cli'."
+    exit
+fi
+
 set -e
 
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
