21d361491be8b2727c23d5d1128ba75a372501e9
Jake Wharton <jw@squareup.com>
2023-02-22 00:10:52 -0500

    Test rendering longer and shorter subsequent outputs

--- mosaic-runtime/src/commonTest/kotlin/com/jakewharton/mosaic/AnsiRenderingTest.kt
+++ mosaic-runtime/src/commonTest/kotlin/com/jakewharton/mosaic/AnsiRenderingTest.kt
@@ -22,4 +22,70 @@ class AnsiRenderingTest {
 			rendering.render(helloCanvas).toString(),
 		)
 	}
+
+	@Test fun subsequentLongerRenderClearsRenderedLines() {
+		val firstCanvas = TextSurface(6, 2).apply {
+			write(0, 0, "Hello")
+			write(1, 0, "World!")
+		}
+
+		assertEquals(
+			"""
+			|Hello $clearLine
+			|World!$clearLine
+			|""".trimMargin(),
+			rendering.render(firstCanvas).toString(),
+		)
+
+		val secondCanvas = TextSurface(3, 4).apply {
+			write(0, 0, "Hel")
+			write(1, 0, "lo")
+			write(2, 0, "Wor")
+			write(3, 0, "ld!")
+		}
+
+		assertEquals(
+			"""
+			|$cursorUp${cursorUp}Hel$clearLine
+			|lo $clearLine
+			|Wor$clearLine
+			|ld!$clearLine
+			|""".trimMargin(),
+			rendering.render(secondCanvas).toString(),
+		)
+	}
+
+	@Test fun subsequentShorterRenderClearsRenderedLines() {
+		val firstCanvas = TextSurface(3, 4).apply {
+			write(0, 0, "Hel")
+			write(1, 0, "lo")
+			write(2, 0, "Wor")
+			write(3, 0, "ld!")
+		}
+
+		assertEquals(
+			"""
+			|Hel$clearLine
+			|lo $clearLine
+			|Wor$clearLine
+			|ld!$clearLine
+			|""".trimMargin(),
+			rendering.render(firstCanvas).toString(),
+		)
+
+		val secondCanvas = TextSurface(6, 2).apply {
+			write(0, 0, "Hello")
+			write(1, 0, "World!")
+		}
+
+		assertEquals(
+			"""
+			|$cursorUp$cursorUp$cursorUp${cursorUp}Hello $clearLine
+			|World!$clearLine
+			|$clearLine
+			|$clearLine$cursorUp
+			""".trimMargin(),
+			rendering.render(secondCanvas).toString(),
+		)
+	}
 }
