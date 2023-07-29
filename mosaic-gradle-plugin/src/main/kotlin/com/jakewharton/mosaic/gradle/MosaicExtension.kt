c82b717dd49f7eb9a397d3519573c9c6a5bbdb2f
Jake Wharton <jw@squareup.com>
2023-07-29 00:36:00 -0400

    Support custom version or coordinates for Compose compiler (#207)
    Since this project does not build a Kotlin compiler plugin, we should not be forced to do a release for new versions of Kotlin. This property will allow consumers to upgrade their Kotlin version by specifying a newer JetBrains Compose compiler version, or a totally different set of Maven coordinates for a custom Compose compiler artifact.

---
+++ mosaic-gradle-plugin/src/main/kotlin/com/jakewharton/mosaic/gradle/MosaicExtension.kt
@@ -0,0 +1,25 @@
+package com.jakewharton.mosaic.gradle
+
+import org.gradle.api.provider.Property
+
+interface MosaicExtension {
+	/**
+	 * The version of the JetBrains Compose compiler to use, or a Maven coordinate triple of
+	 * the custom Compose compiler to use.
+	 *
+	 * Example: using a custom version of the JetBrains Compose compiler
+	 * ```kotlin
+	 * redwood {
+	 *   kotlinCompilerPlugin.set("1.4.8")
+	 * }
+	 * ```
+	 *
+	 * Example: using a custom Maven coordinate for the Compose compiler
+	 * ```kotlin
+	 * redwood {
+	 *   kotlinCompilerPlugin.set("com.example:custom-compose-compiler:1.0.0")
+	 * }
+	 * ```
+	 */
+	val kotlinCompilerPlugin: Property<String>
+}
--- mosaic-gradle-plugin/src/test/kotlin/com/jakewharton/mosaic/gradle/FixtureTest.kt
+++ mosaic-gradle-plugin/src/test/kotlin/com/jakewharton/mosaic/gradle/FixtureTest.kt
@@ -1,36 +1,45 @@
 package com.jakewharton.mosaic.gradle
 
+import com.google.common.truth.Truth.assertThat
+import java.io.File
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
-import org.junit.runners.Parameterized.Parameters
-import java.io.File
 
-@RunWith(Parameterized::class)
-class FixtureTest(
-	private val fixtureName: String,
-) {
-	companion object {
-		@JvmStatic
-		@Parameters(name = "{0}")
-		fun parameters() = listOf(
-			arrayOf("counter"),
+class FixtureTest {
+	@Test fun counter() {
+		fixture("counter").build()
+	}
+
+	@Test fun customCompilerCoordinates() {
+		fixture("custom-compiler-coordinates").build()
+	}
+
+	@Test fun customCompilerInvalid() {
+		val result = fixture("custom-compiler-invalid").buildAndFail()
+		assertThat(result.output).contains(
+			"""
+			|Illegal format of 'mosaic.kotlinCompilerPlugin' property.
+			|Expected format: either '<VERSION>' or '<GROUP_ID>:<ARTIFACT_ID>:<VERSION>'
+			|Actual value: 'wrong:format'
+			""".trimMargin(),
 		)
 	}
 
-	private val fixturesDir = File("src/test/fixture")
-	private fun versionProperty() = "-PmosaicVersion=$mosaicVersion"
+	@Test fun customCompilerVersion() {
+		fixture("custom-compiler-version").build()
+	}
 
-	@Test fun todo() {
-		val fixtureDir = File(fixturesDir, fixtureName)
-		val gradleRoot = File(fixtureDir, "gradle").also { it.mkdir() }
-		File("../gradle").copyRecursively(File(gradleRoot.path), true)
+	private fun fixture(
+		name: String,
+		tasks: Array<String> = arrayOf("clean", "build"),
+	): GradleRunner {
+		val fixtureDir = File("src/test/fixture", name)
+		val gradleWrapper = File(fixtureDir, "gradle/wrapper").also { it.mkdirs() }
+		File("../gradle/wrapper").copyRecursively(File(gradleWrapper.path), true)
 
-		GradleRunner.create()
+		return GradleRunner.create()
 			.withProjectDir(fixtureDir)
-			.withArguments("clean", "build", "--stacktrace", versionProperty())
-			.withDebug(true) // Run in-process for speed.
-			.build()
+			.withArguments(*tasks, "--stacktrace", "-PmosaicVersion=$mosaicVersion")
+			.withDebug(true)
 	}
 }
