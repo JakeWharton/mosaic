4907bbb3c2b6a3e2ee369dd1d24e5c232d1e5d50
Jake Wharton <jw@squareup.com>
2023-07-29 00:20:11 -0400

    Support custom version or coordinates for Compose compiler
    Since this project does not build a Kotlin compiler plugin, we should not be forced to do a release for new versions of Kotlin. This property will allow consumers to upgrade thier Kotlin version by specifying a newer JetBrains Compose compiler version, or a totally different set of Maven coordiantes for a custom Compose compiler aritfact.

--- mosaic-gradle-plugin/src/main/kotlin/com/jakewharton/mosaic/gradle/MosaicPlugin.kt
+++ mosaic-gradle-plugin/src/main/kotlin/com/jakewharton/mosaic/gradle/MosaicPlugin.kt
@@ -1,7 +1,10 @@
 package com.jakewharton.mosaic.gradle
 
+import javax.inject.Inject
 import org.gradle.api.Project
+import org.gradle.api.model.ObjectFactory
 import org.gradle.api.plugins.JavaPlugin.API_CONFIGURATION_NAME
+import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
@@ -12,32 +15,27 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet.Companion.COMMON_MAIN_
 import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
 import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
 
+private open class MosaicExtensionImpl
+@Inject constructor(objectFactory: ObjectFactory) : MosaicExtension {
+	override val kotlinCompilerPlugin: Property<String> =
+		objectFactory.property(String::class.java)
+			.convention(composeCompilerVersion)
+}
+
+private const val extensionName = "mosaic"
+
+@Suppress("unused") // Used reflectively by Gradle.
 class MosaicPlugin : KotlinCompilerPluginSupportPlugin {
-	override fun isApplicable(kotlinCompilation: KotlinCompilation<*>) = true
-
-	override fun getCompilerPluginId() = "com.jakewharton.mosaic"
-
-	override fun getPluginArtifact() = SubpluginArtifact(
-		composeCompilerGroupId,
-		composeCompilerArtifactId,
-		composeCompilerVersion,
-	)
-
-	override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
-		if (kotlinCompilation.target.platformType == KotlinPlatformType.js) {
-			// This enables a workaround for Compose lambda generation to function correctly in JS.
-			// Note: We cannot use SubpluginOption to do this because it targets the Compose plugin.
-			kotlinCompilation.compilerOptions.options.freeCompilerArgs.addAll(
-				"-P",
-				"plugin:androidx.compose.compiler.plugins.kotlin:generateDecoys=true",
-			)
-		}
-
-		return kotlinCompilation.target.project.provider { emptyList() }
-	}
+	private lateinit var extension: MosaicExtension
 
 	override fun apply(target: Project) {
 		super.apply(target)
+
+		extension = target.extensions.create(
+			MosaicExtension::class.java,
+			extensionName,
+			MosaicExtensionImpl::class.java,
+		)
 
 		if (target.isInternal() && target.path == ":mosaic-runtime") {
 			// Being lazy and using our own plugin to configure the Compose compiler on our runtime.
@@ -67,6 +65,39 @@ class MosaicPlugin : KotlinCompilerPluginSupportPlugin {
 				throw IllegalStateException("Kotlin/JVM or Kotlin/Multiplatform plugin must be applied.")
 			}
 		}
+	}
+
+	override fun isApplicable(kotlinCompilation: KotlinCompilation<*>) = true
+
+	override fun getCompilerPluginId() = "com.jakewharton.mosaic"
+
+	override fun getPluginArtifact(): SubpluginArtifact {
+		val plugin = extension.kotlinCompilerPlugin.get()
+		val parts = plugin.split(":")
+		return when (parts.size) {
+			1 -> SubpluginArtifact("org.jetbrains.compose.compiler", "compiler", parts[0])
+			3 -> SubpluginArtifact(parts[0], parts[1], parts[2])
+			else -> error(
+				"""
+        |Illegal format of '$extensionName.${MosaicExtension::kotlinCompilerPlugin.name}' property.
+        |Expected format: either '<VERSION>' or '<GROUP_ID>:<ARTIFACT_ID>:<VERSION>'
+        |Actual value: '$plugin'
+        """.trimMargin(),
+			)
+		}
+	}
+
+	override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
+		if (kotlinCompilation.target.platformType == KotlinPlatformType.js) {
+			// This enables a workaround for Compose lambda generation to function correctly in JS.
+			// Note: We cannot use SubpluginOption to do this because it targets the Compose plugin.
+			kotlinCompilation.compilerOptions.options.freeCompilerArgs.addAll(
+				"-P",
+				"plugin:androidx.compose.compiler.plugins.kotlin:generateDecoys=true",
+			)
+		}
+
+		return kotlinCompilation.target.project.provider { emptyList() }
 	}
 
 	private fun Project.isInternal(): Boolean {
