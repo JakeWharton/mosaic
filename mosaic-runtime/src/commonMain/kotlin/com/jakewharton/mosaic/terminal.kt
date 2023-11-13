87e0fab32dbb88fed67aec0a002d1ad919e62815
Jake Wharton <jw@squareup.com>
2023-11-13 12:12:18 -0500

    Migrate to Poko from data classes
    At least for those in public API.

--- mosaic-runtime/src/commonMain/kotlin/com/jakewharton/mosaic/terminal.kt
+++ mosaic-runtime/src/commonMain/kotlin/com/jakewharton/mosaic/terminal.kt
@@ -1,17 +1,21 @@
 package com.jakewharton.mosaic
 
 import androidx.compose.runtime.ProvidableCompositionLocal
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.compositionLocalOf
+import dev.drewhamilton.poko.Poko
 
 public val Terminal: ProvidableCompositionLocal<TerminalInfo> = compositionLocalOf {
 	error("No terminal info")
 }
 
-public data class TerminalInfo(
-	val size: Size,
+@[Stable Poko]
+public class TerminalInfo(
+	public val size: Size,
 ) {
-	public data class Size(
-		val width: Int,
-		val height: Int,
+	@[Stable Poko]
+	public class Size(
+		public val width: Int,
+		public val height: Int,
 	)
 }
