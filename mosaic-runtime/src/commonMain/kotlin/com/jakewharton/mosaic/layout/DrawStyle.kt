148502f81526885c25bd489aacc9a15b01ed4fe0
EpicDima <37583380+EpicDima@users.noreply.github.com>
2024-05-30 14:49:11 +0300

    Some code fixes (#384)
    - Move `Fill` and `Stroke` inside the `DrawStyle`
    - Remove `toString` from `DrawStyle.Stroke` because `@Poko` is already doing it
    - In the robot sample, the use of the `offset` modifier in the robot (`Text`)
    - Remove `TODO https://github.com/JakeWharton/mosaic/issues/11` in the robot sample, it seems that it is no longer relevant
    - In the robot sample, add `isActive` check in the loop for keyboard input
    - Fix in the `README.md` `mutableStateOf` to `mutableIntStateOf`, as is correct in newer versions

--- mosaic-runtime/src/commonMain/kotlin/com/jakewharton/mosaic/layout/DrawStyle.kt
+++ mosaic-runtime/src/commonMain/kotlin/com/jakewharton/mosaic/layout/DrawStyle.kt
@@ -5,24 +5,19 @@ import dev.drewhamilton.poko.Poko
 /**
  * Represents how the shapes should be drawn within a [DrawScope]
  */
-public sealed interface DrawStyle
+public sealed interface DrawStyle {
 
-/**
- * Default [DrawStyle] indicating shapes should be drawn completely filled in with the
- * provided color or pattern
- */
-public data object Fill : DrawStyle
+	/**
+	 * Default [DrawStyle] indicating shapes should be drawn completely filled in with the
+	 * provided color or pattern
+	 */
+	public data object Fill : DrawStyle
 
-/**
- * [DrawStyle] that provides information for drawing content with a stroke
- *
- * @param width Configure the width of the stroke in cells
- */
-@Poko
-public class Stroke(
-	internal val width: Int = 1,
-) : DrawStyle {
-	override fun toString(): String {
-		return "Stroke(width=$width)"
-	}
+	/**
+	 * [DrawStyle] that provides information for drawing content with a stroke
+	 *
+	 * @param width Configure the width of the stroke in cells
+	 */
+	@Poko
+	public class Stroke(internal val width: Int = 1) : DrawStyle
 }
