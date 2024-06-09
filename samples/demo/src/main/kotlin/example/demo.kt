4d6fd10578fed2ac612c24dd1f6270e31e3f92e4
EpicDima <dima2-8@bk.ru>
2024-06-10 00:47:30 +0300

    Some fun animation in demo sample

--- samples/demo/src/main/kotlin/example/demo.kt
+++ samples/demo/src/main/kotlin/example/demo.kt
@@ -1,6 +1,12 @@
 package example
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.jakewharton.mosaic.LocalTerminal
 import com.jakewharton.mosaic.layout.background
 import com.jakewharton.mosaic.layout.height
@@ -13,12 +19,14 @@ import com.jakewharton.mosaic.text.withStyle
 import com.jakewharton.mosaic.ui.Box
 import com.jakewharton.mosaic.ui.Color
 import com.jakewharton.mosaic.ui.Column
+import com.jakewharton.mosaic.ui.ColumnScope
 import com.jakewharton.mosaic.ui.Filler
 import com.jakewharton.mosaic.ui.Row
 import com.jakewharton.mosaic.ui.Spacer
 import com.jakewharton.mosaic.ui.Text
 import com.jakewharton.mosaic.ui.TextStyle
 import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.delay
 
 private val BrightGreen = Color(100, 255, 100)
 private val BrightBlue = Color(60, 140, 230)
@@ -35,14 +43,24 @@ fun main() = runMosaicBlocking {
 					withStyle(SpanStyle(color = BrightGreen)) {
 						append("width=")
 					}
-					withStyle(SpanStyle(color = BrightBlue, textStyle = TextStyle.Bold + TextStyle.Underline)) {
+					withStyle(
+						SpanStyle(
+							color = BrightBlue,
+							textStyle = TextStyle.Bold + TextStyle.Underline,
+						),
+					) {
 						append(terminal.size.width.toString())
 					}
 					append(", ")
 					withStyle(SpanStyle(color = BrightGreen)) {
 						append("height=")
 					}
-					withStyle(SpanStyle(color = BrightBlue, textStyle = TextStyle.Bold + TextStyle.Underline)) {
+					withStyle(
+						SpanStyle(
+							color = BrightBlue,
+							textStyle = TextStyle.Bold + TextStyle.Underline,
+						),
+					) {
 						append(terminal.size.height.toString())
 					}
 					append(")")
@@ -51,37 +69,76 @@ fun main() = runMosaicBlocking {
 				},
 			)
 			Spacer(modifier = Modifier.height(1))
-			Gradient(
-				repeatedWord = "Red",
-				textColorProvider = { percent -> Color(1.0f - percent, 0.0f, 0.0f) },
-				backgroundColorProvider = { percent -> Color(percent, 0.0f, 0.0f) },
-			)
-			Gradient(
-				repeatedWord = "Green",
-				textColorProvider = { percent -> Color(0.0f, 1.0f - percent, 0.0f) },
-				backgroundColorProvider = { percent -> Color(0.0f, percent, 0.0f) },
-			)
-			Gradient(
-				repeatedWord = "Blue",
-				textColorProvider = { percent -> Color(0.0f, 0.0f, 1.0f - percent) },
-				backgroundColorProvider = { percent -> Color(0.0f, 0.0f, percent) },
-			)
+			GradientsBlock()
 		}
 	}
 
 	awaitCancellation()
 }
 
+@Suppress("UnusedReceiverParameter") // instead of ignore rule: compose:multiple-emitters-check
+@Composable
+private fun ColumnScope.GradientsBlock() {
+	val screenHalfWidth = LocalTerminal.current.size.width / 2
+	var gradientWidth by remember { mutableIntStateOf(0) }
+	val gradientWidthDiff by remember(screenHalfWidth) {
+		derivedStateOf { (screenHalfWidth - gradientWidth) / 5 }
+	}
+	Gradient(
+		repeatedWord = "Red",
+		width = gradientWidth,
+		textColorProvider = { percent -> Color(1.0f - percent, 0.0f, 0.0f) },
+		backgroundColorProvider = { percent -> Color(percent, 0.0f, 0.0f) },
+	)
+	Gradient(
+		repeatedWord = "Yellow",
+		width = gradientWidth,
+		textColorProvider = { percent -> Color(1.0f - percent, 1.0f - percent, 0.0f) },
+		backgroundColorProvider = { percent -> Color(percent, percent, 0.0f) },
+	)
+	Gradient(
+		repeatedWord = "Green",
+		width = gradientWidth,
+		textColorProvider = { percent -> Color(0.0f, 1.0f - percent, 0.0f) },
+		backgroundColorProvider = { percent -> Color(0.0f, percent, 0.0f) },
+	)
+	Gradient(
+		repeatedWord = "Cyan",
+		width = gradientWidth,
+		textColorProvider = { percent -> Color(0.0f, 1.0f - percent, 1.0f - percent) },
+		backgroundColorProvider = { percent -> Color(0.0f, percent, percent) },
+	)
+	Gradient(
+		repeatedWord = "Blue",
+		width = gradientWidth,
+		textColorProvider = { percent -> Color(0.0f, 0.0f, 1.0f - percent) },
+		backgroundColorProvider = { percent -> Color(0.0f, 0.0f, percent) },
+	)
+	Gradient(
+		repeatedWord = "Magenta",
+		width = gradientWidth,
+		textColorProvider = { percent -> Color(1.0f - percent, 0.0f, 1.0f - percent) },
+		backgroundColorProvider = { percent -> Color(percent, 0.0f, percent) },
+	)
+	LaunchedEffect(screenHalfWidth) {
+		while (true) {
+			delay(100L)
+			gradientWidth += gradientWidthDiff
+		}
+	}
+}
+
 @Composable
 private fun Gradient(
 	repeatedWord: String,
+	width: Int,
 	textColorProvider: (percent: Float) -> Color,
 	backgroundColorProvider: (percent: Float) -> Color,
 ) {
-	val width = LocalTerminal.current.size.width / 2
+	var textBias by remember { mutableIntStateOf(0) }
 	Box {
 		Row {
-			var wordCharIndex = 0
+			var wordCharIndex = textBias
 			repeat(width) { index ->
 				if (wordCharIndex == repeatedWord.length) {
 					wordCharIndex = 0
@@ -101,6 +158,15 @@ private fun Gradient(
 						.size(1)
 						.background(backgroundColorProvider.invoke(index / width.toFloat())),
 				)
+			}
+		}
+	}
+	LaunchedEffect(Unit) {
+		while (true) {
+			delay(200L)
+			textBias--
+			if (textBias < 0) {
+				textBias = repeatedWord.length - 1
 			}
 		}
 	}
