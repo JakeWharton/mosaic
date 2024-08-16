d24b8cd8803913d2a889694bac6ad81527aba754
EpicDima <37583380+EpicDima@users.noreply.github.com>
2024-08-17 02:14:25 +0300

    Add unit test for counter (#397)

--- mosaic-runtime/src/commonTest/kotlin/com/jakewharton/mosaic/MosaicTest.kt
+++ mosaic-runtime/src/commonTest/kotlin/com/jakewharton/mosaic/MosaicTest.kt
@@ -1,10 +1,17 @@
 package com.jakewharton.mosaic
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.jakewharton.mosaic.ui.Column
 import com.jakewharton.mosaic.ui.Text
 import kotlin.test.Test
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runTest
 
 class MosaicTest {
 	@Test fun render() {
@@ -23,5 +30,43 @@ class MosaicTest {
 			|
 			""".trimMargin().replaceLineEndingsWithCRLF(),
 		)
+	}
+
+	@Test fun counter() = runTest {
+		val strings = mutableListOf<CharSequence>()
+
+		runMosaic(
+			content = {
+				var count by remember { mutableIntStateOf(0) }
+
+				Text("The count is: $count")
+
+				LaunchedEffect(Unit) {
+					for (i in 1..20) {
+						delay(250)
+						count = i
+					}
+				}
+			},
+			display = {
+				strings.add(it)
+			},
+		)
+
+		assertThat(strings.size).isEqualTo(21)
+		assertThat(strings[0]).isEqualTo(
+			"""
+			|${ansiBeginSynchronizedUpdate}The count is: 0
+			|$ansiEndSynchronizedUpdate
+			""".trimMargin().replaceLineEndingsWithCRLF(),
+		)
+		for (i in 1..20) {
+			assertThat(strings[i]).isEqualTo(
+				"""
+				|${ansiBeginSynchronizedUpdate}${cursorUp}The count is: ${i}$clearLine
+				|$ansiEndSynchronizedUpdate
+				""".trimMargin().replaceLineEndingsWithCRLF(),
+			)
+		}
 	}
 }
