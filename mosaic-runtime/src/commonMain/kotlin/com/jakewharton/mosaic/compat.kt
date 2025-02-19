4ab133669400fb834bceb7eea2789ecd25077248
Jake Wharton <jw@squareup.com>
2025-02-18 22:18:25 -0500

    Map all the keys on my keyboard
    After this I cannot get it to crash.

--- mosaic-runtime/src/commonMain/kotlin/com/jakewharton/mosaic/compat.kt
+++ mosaic-runtime/src/commonMain/kotlin/com/jakewharton/mosaic/compat.kt
@@ -12,6 +12,7 @@ internal fun KeyboardEvent.toKeyEventOrNull(): KeyEvent? {
 		key = when (val codepoint = codepoint) {
 			9 -> "Tab"
 			13 -> "Enter"
+			27 -> "Escape"
 			in 32..126 -> codepoint.toChar().toString()
 			127 -> "Backspace"
 			57350 -> "ArrowLeft"
@@ -20,6 +21,10 @@ internal fun KeyboardEvent.toKeyEventOrNull(): KeyEvent? {
 			57353 -> "ArrowDown"
 			57348 -> "Insert"
 			57349 -> "Delete"
+			57354 -> "PageUp"
+			57355 -> "PageDown"
+			57356 -> "Home"
+			57357 -> "End"
 			in 57364..57398 -> "F" + (codepoint - 57363)
 			else -> throw UnsupportedOperationException(toString())
 		},
