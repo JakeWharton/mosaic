dde0583ffd1ad59663f63302435efdf505aa1f4d
EdwarDDay <4127904+EdwarDDay@users.noreply.github.com>
2025-03-24 17:17:49 +0100

    Make rrtop sample working in MPP
    - fix formatting violations

--- samples/rrtop/src/jvmMain/kotlin/example/stringFormat.kt
+++ samples/rrtop/src/jvmMain/kotlin/example/stringFormat.kt
@@ -3,19 +3,19 @@ package example
 import kotlin.text.format as jvmFormat
 
 actual fun String.Companion.format(formatString: String, formatArg: Float): String =
-    String.jvmFormat(formatString, formatArg)
+	String.jvmFormat(formatString, formatArg)
 
 actual fun String.Companion.format(formatString: String, formatArg: Double): String =
-    String.jvmFormat(formatString, formatArg)
+	String.jvmFormat(formatString, formatArg)
 
 actual fun String.Companion.format(formatString: String, formatArg1: Int, formatArg2: Long, formatArg3: Long): String =
-    String.jvmFormat(formatString, formatArg1, formatArg2, formatArg3)
+	String.jvmFormat(formatString, formatArg1, formatArg2, formatArg3)
 
 actual fun String.Companion.format(
-    formatString: String,
-    formatArg1: Long,
-    formatArg2: Long,
-    formatArg3: Long,
-    formatArg4: Long,
+	formatString: String,
+	formatArg1: Long,
+	formatArg2: Long,
+	formatArg3: Long,
+	formatArg4: Long,
 ): String =
-    String.jvmFormat(formatString, formatArg1, formatArg2, formatArg3, formatArg4)
+	String.jvmFormat(formatString, formatArg1, formatArg2, formatArg3, formatArg4)
