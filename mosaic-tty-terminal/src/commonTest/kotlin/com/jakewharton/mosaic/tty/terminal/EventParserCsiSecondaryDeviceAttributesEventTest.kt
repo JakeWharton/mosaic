3f2c6107e78ea45a93e2b69660f354884edc521a
Jake Wharton <github@jakewharton.com>
2025-03-25 11:59:41 -0400

    Parse secondary attributes event (#827)

---
+++ mosaic-tty-terminal/src/commonTest/kotlin/com/jakewharton/mosaic/tty/terminal/EventParserCsiSecondaryDeviceAttributesEventTest.kt
@@ -0,0 +1,79 @@
+package com.jakewharton.mosaic.tty.terminal
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.jakewharton.mosaic.terminal.SecondaryDeviceAttributesEvent
+import com.jakewharton.mosaic.terminal.UnknownEvent
+import kotlin.test.Test
+
+class EventParserCsiSecondaryDeviceAttributesEventTest : BaseEventParserTest() {
+	@Test fun emptyFails() {
+		testTty.writeHex("1b5b3e63")
+		assertThat(parser.next()).isEqualTo(
+			UnknownEvent("1b5b3e63".hexToByteArray()),
+		)
+	}
+
+	@Test fun missingFirmwareVersionAndRegistrationNumberFails() {
+		testTty.writeHex("1b5b3e3163")
+		assertThat(parser.next()).isEqualTo(
+			UnknownEvent("1b5b3e3163".hexToByteArray()),
+		)
+	}
+
+	@Test fun missingRegistrationNumberFails() {
+		testTty.writeHex("1b5b3e313b313063")
+		assertThat(parser.next()).isEqualTo(
+			UnknownEvent("1b5b3e313b313063".hexToByteArray()),
+		)
+	}
+
+	@Test fun emptyTypeFails() {
+		testTty.writeHex("1b5b3e3b31303b3063")
+		assertThat(parser.next()).isEqualTo(
+			UnknownEvent("1b5b3e3b31303b3063".hexToByteArray()),
+		)
+	}
+
+	@Test fun emptyFirmwareVersionFails() {
+		testTty.writeHex("1b5b3e313b3b3063")
+		assertThat(parser.next()).isEqualTo(
+			UnknownEvent("1b5b3e313b3b3063".hexToByteArray()),
+		)
+	}
+
+	@Test fun emptyRegistrationNumberFails() {
+		testTty.writeHex("1b5b3e313b31303b63")
+		assertThat(parser.next()).isEqualTo(
+			UnknownEvent("1b5b3e313b31303b63".hexToByteArray()),
+		)
+	}
+
+	@Test fun nonDigitTypeFails() {
+		testTty.writeHex("1b5b3e3a3b31303b3063")
+		assertThat(parser.next()).isEqualTo(
+			UnknownEvent("1b5b3e3a3b31303b3063".hexToByteArray()),
+		)
+	}
+
+	@Test fun nonDigitFirmwareVersionFails() {
+		testTty.writeHex("1b5b3e313b3a3b3063")
+		assertThat(parser.next()).isEqualTo(
+			UnknownEvent("1b5b3e313b3a3b3063".hexToByteArray()),
+		)
+	}
+
+	@Test fun nonDigitRegistrationNumberFails() {
+		testTty.writeHex("1b5b3e313b30313b3a63")
+		assertThat(parser.next()).isEqualTo(
+			UnknownEvent("1b5b3e313b30313b3a63".hexToByteArray()),
+		)
+	}
+
+	@Test fun valid() {
+		testTty.writeHex("1b5b3e313b31303b3063")
+		assertThat(parser.next()).isEqualTo(
+			SecondaryDeviceAttributesEvent(1, 10, 0),
+		)
+	}
+}
