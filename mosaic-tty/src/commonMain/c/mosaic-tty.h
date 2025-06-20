da4596cbeb58fa88f14257ec0af73c904f784ca6
Jake Wharton <jw@squareup.com>
2025-06-20 13:09:39 -0400

    WIP

--- mosaic-tty/src/commonMain/c/mosaic-tty.h
+++ mosaic-tty/src/commonMain/c/mosaic-tty.h
@@ -39,16 +39,16 @@ typedef struct MosaicTtyTerminalSizeResult {
 	uint32_t error;
 } MosaicTtyTerminalSizeResult;
 
-MosaicTtyInitResult tty_init();
-void tty_setCallback(MosaicTty *tty, MosaicTtyCallback *callback);
-MosaicTtyIoResult tty_read(MosaicTty *tty, uint8_t *buffer, int count);
-MosaicTtyIoResult tty_readWithTimeout(MosaicTty *tty, uint8_t *buffer, int count, int timeoutMillis);
-uint32_t tty_interruptRead(MosaicTty *tty);
-MosaicTtyIoResult tty_write(MosaicTty *tty, uint8_t *buffer, int count);
-uint32_t tty_enableRawMode(MosaicTty *tty);
-uint32_t tty_enableWindowResizeEvents(MosaicTty *tty);
-MosaicTtyTerminalSizeResult tty_currentTerminalSize(MosaicTty *tty);
-uint32_t tty_reset(MosaicTty *tty);
-uint32_t tty_free(MosaicTty *tty);
+extern MosaicTtyInitResult tty_init();
+extern void tty_setCallback(MosaicTty *tty, MosaicTtyCallback *callback);
+extern MosaicTtyIoResult tty_read(MosaicTty *tty, uint8_t *buffer, int count);
+extern MosaicTtyIoResult tty_readWithTimeout(MosaicTty *tty, uint8_t *buffer, int count, int timeoutMillis);
+extern uint32_t tty_interruptRead(MosaicTty *tty);
+extern MosaicTtyIoResult tty_write(MosaicTty *tty, uint8_t *buffer, int count);
+extern uint32_t tty_enableRawMode(MosaicTty *tty);
+extern uint32_t tty_enableWindowResizeEvents(MosaicTty *tty);
+extern MosaicTtyTerminalSizeResult tty_currentTerminalSize(MosaicTty *tty);
+extern uint32_t tty_reset(MosaicTty *tty);
+extern uint32_t tty_free(MosaicTty *tty);
 
 #endif // MOSAIC_TTY_H
