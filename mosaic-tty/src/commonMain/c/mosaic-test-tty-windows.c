7898272de69c73c92049c9e2d2d32b5825259dc8
Jake Wharton <jw@squareup.com>
2025-06-20 13:29:36 -0400

    WIP

--- mosaic-tty/src/commonMain/c/mosaic-test-tty-windows.c
+++ mosaic-tty/src/commonMain/c/mosaic-test-tty-windows.c
@@ -28,7 +28,7 @@ uint32_t testTty_resizeInternal(HANDLE conout, int columns, int rows) {
 	return GetLastError();
 }
 
-extern MosaicTestTtyInitResult testTty_init() {
+__declspec(dllexport) MosaicTestTtyInitResult __stdcall testTty_init() {
 	MosaicTestTtyInitResult result = {};
 
 	MosaicTestTtyImpl *testTty = calloc(1, sizeof(MosaicTestTtyImpl));
