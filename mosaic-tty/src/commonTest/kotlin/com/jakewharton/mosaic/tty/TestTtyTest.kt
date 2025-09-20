aaec69c8a47b1d31e1afef693c08184bbf8cbee2
Jake Wharton <github@jakewharton.com>
2025-09-19 23:34:22 -0400

    Unify the read/write tests (#975)
    This ensures we're testing the same thing from both directions, and can reuse it for testing stdin, stdout, and stderr manipulation in the future.

--- mosaic-tty/src/commonTest/kotlin/com/jakewharton/mosaic/tty/TestTtyTest.kt
+++ mosaic-tty/src/commonTest/kotlin/com/jakewharton/mosaic/tty/TestTtyTest.kt
@@ -6,14 +6,8 @@ import assertk.assertThat
 import assertk.assertions.hasMessage
 import assertk.assertions.isEqualTo
 import assertk.assertions.isInstanceOf
-import assertk.assertions.isZero
 import kotlin.test.AfterTest
 import kotlin.test.Test
-import kotlin.time.Duration.Companion.milliseconds
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.runTest
 
 @Burst
 class TestTtyTest {
@@ -52,88 +46,6 @@ class TestTtyTest {
 			tty.reset()
 			tty.enableRawMode()
 		}
-	}
-
-	@Test fun readWhatWasWritten() {
-		tty.write("hello")
-
-		val bufferA = ByteArray(10) { 'x'.code.toByte() }
-		val readA = testTty.read(bufferA, 0, 10)
-		assertThat(readA, "readA").isEqualTo(5)
-		assertThat(bufferA.decodeToString()).isEqualTo("helloxxxxx")
-
-		tty.write("world")
-
-		val bufferB = ByteArray(10) { 'x'.code.toByte() }
-		val readB = testTty.read(bufferB, 0, 10)
-		assertThat(readB, "readB").isEqualTo(5)
-		assertThat(bufferB.decodeToString()).isEqualTo("worldxxxxx")
-	}
-
-	@Test fun readOnlyUpToCount() {
-		tty.write("abcdefghij")
-
-		val buffer = ByteArray(10) { 'x'.code.toByte() }
-		val read = testTty.read(buffer, 0, 5)
-		assertThat(read).isEqualTo(5)
-		assertThat(buffer.decodeToString()).isEqualTo("abcdexxxxx")
-
-		// Drain the TTY output buffer, otherwise resetting raw mode will hang on its flush.
-		testTty.read(5)
-	}
-
-	@Test fun readUnderflow() {
-		tty.write("hello")
-
-		val buffer = ByteArray(10) { 'x'.code.toByte() }
-		val read = testTty.read(buffer, 0, 10)
-		assertThat(read).isEqualTo(5)
-		assertThat(buffer.decodeToString()).isEqualTo("helloxxxxx")
-	}
-
-	@Test fun readAtOffset() {
-		tty.write("hello")
-
-		val buffer = ByteArray(10) { 'x'.code.toByte() }
-		val read = testTty.read(buffer, 5, 5)
-		assertThat(read).isEqualTo(5)
-		assertThat(buffer.decodeToString()).isEqualTo("xxxxxhello")
-	}
-
-	@Test fun readCanBeInterrupted() = runTest {
-		backgroundScope.launch(Dispatchers.Default) {
-			delay(150.milliseconds)
-			testTty.interruptRead()
-		}
-		val readA = testTty.read(ByteArray(10), 0, 10)
-		assertThat(readA).isZero()
-
-		backgroundScope.launch(Dispatchers.Default) {
-			delay(150.milliseconds)
-			testTty.interruptRead()
-		}
-		val readB = testTty.read(ByteArray(10), 0, 10)
-		assertThat(readB).isZero()
-	}
-
-	@Test fun writeOnlyUpToCount() {
-		val written = testTty.write("abcdefghij".encodeToByteArray(), 0, 5)
-		assertThat(written).isEqualTo(5)
-
-		val buffer = ByteArray(10) { 'x'.code.toByte() }
-		val read = tty.read(buffer, 0, 10)
-		assertThat(read).isEqualTo(5)
-		assertThat(buffer.decodeToString()).isEqualTo("abcdexxxxx")
-	}
-
-	@Test fun writeAtOffset() {
-		val written = testTty.write("abcdefghij".encodeToByteArray(), 5, 5)
-		assertThat(written).isEqualTo(5)
-
-		val buffer = ByteArray(10) { 'x'.code.toByte() }
-		val read = tty.read(buffer, 0, 10)
-		assertThat(read).isEqualTo(5)
-		assertThat(buffer.decodeToString()).isEqualTo("fghijxxxxx")
 	}
 
 	@Test fun stdinIsTtySetting(value: Boolean) {
