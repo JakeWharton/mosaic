8a9050057316d12b00b9e0ba1902200cfa00e5f4
Jake Wharton <jw@squareup.com>
2025-09-24 09:46:55 -0400

    Split data tests into reader and writer

--- mosaic-tty/src/commonTest/kotlin/com/jakewharton/mosaic/tty/DataWriterTest.kt
+++ mosaic-tty/src/commonTest/kotlin/com/jakewharton/mosaic/tty/DataWriterTest.kt
@@ -2,32 +2,30 @@ package com.jakewharton.mosaic.tty
 
 import app.cash.burst.Burst
 import app.cash.burst.InterceptTest
+import app.cash.burst.burstValues
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import kotlin.test.Test
 
 @Burst
-class DataWriterTest(dataPipe: DataPipe) {
+class DataWriterTest(
 	@InterceptTest
-	private val rw = dataPipe.createInterceptor()
-
+	private val data: DataWriter = burstValues(
+		TtyToTestTty,
+		TestTtyToTest,
+	),
+) {
 	@Test fun writeOnlyUpToCount() {
-		val written = rw.write("abcdefghij".encodeToByteArray(), 0, 5)
+		val written = data.write("abcdefghij".encodeToByteArray(), 0, 5)
 		assertThat(written).isEqualTo(5)
 
-		val buffer = ByteArray(10) { 'x'.code.toByte() }
-		val read = rw.read(buffer, 0, 10)
-		assertThat(read).isEqualTo(5)
-		assertThat(buffer.decodeToString()).isEqualTo("abcdexxxxx")
+		assertThat(data.read(10)).isEqualTo("abcde")
 	}
 
 	@Test fun writeAtOffset() {
-		val written = rw.write("abcdefghij".encodeToByteArray(), 5, 5)
+		val written = data.write("abcdefghij".encodeToByteArray(), 5, 5)
 		assertThat(written).isEqualTo(5)
 
-		val buffer = ByteArray(10) { 'x'.code.toByte() }
-		val read = rw.read(buffer, 0, 10)
-		assertThat(read).isEqualTo(5)
-		assertThat(buffer.decodeToString()).isEqualTo("fghijxxxxx")
+		assertThat(data.read(10)).isEqualTo("fghij")
 	}
 }
