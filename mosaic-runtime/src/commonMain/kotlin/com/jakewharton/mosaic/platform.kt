a8dcc59b5ab9a26a1b5b5b12c0d142cb62394529
Goooler <wangzongler@gmail.com>
2026-01-14 11:21:10 +0800

    Use Kotlin builtin AtomicBoolean
    Since Kotlin 2.1

--- mosaic-runtime/src/commonMain/kotlin/com/jakewharton/mosaic/platform.kt
+++ mosaic-runtime/src/commonMain/kotlin/com/jakewharton/mosaic/platform.kt
@@ -2,14 +2,6 @@ package com.jakewharton.mosaic
 
 internal expect fun env(name: String): String?
 
-internal expect class AtomicBoolean
-
-internal expect inline fun AtomicBoolean.set(value: Boolean)
-
-internal expect inline fun AtomicBoolean.compareAndSet(expect: Boolean, update: Boolean): Boolean
-
-internal expect inline fun atomicBooleanOf(initialValue: Boolean): AtomicBoolean
-
 internal expect inline fun nanoTime(): Long
 
 internal expect fun nonInteractiveExit(): Nothing
--- mosaic-runtime/src/jvmMain/kotlin/com/jakewharton/mosaic/platform.kt
+++ mosaic-runtime/src/jvmMain/kotlin/com/jakewharton/mosaic/platform.kt
@@ -6,23 +6,6 @@ internal actual fun env(name: String): String? {
 	return System.getenv(name)
 }
 
-internal actual typealias AtomicBoolean = java.util.concurrent.atomic.AtomicBoolean
-
-@Suppress("NOTHING_TO_INLINE", "EXTENSION_SHADOWED_BY_MEMBER")
-internal actual inline fun AtomicBoolean.set(value: Boolean) {
-	set(value)
-}
-
-@Suppress("NOTHING_TO_INLINE", "EXTENSION_SHADOWED_BY_MEMBER")
-internal actual inline fun AtomicBoolean.compareAndSet(expect: Boolean, update: Boolean): Boolean {
-	return compareAndSet(expect, update)
-}
-
-@Suppress("NOTHING_TO_INLINE")
-internal actual inline fun atomicBooleanOf(initialValue: Boolean): AtomicBoolean {
-	return AtomicBoolean(initialValue)
-}
-
 @Suppress("NOTHING_TO_INLINE")
 internal actual inline fun nanoTime(): Long = System.nanoTime()
 
